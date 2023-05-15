ae0d36e048f54a23ff1621bfb405f72d89b7d3ff
azzaval <azzaval@elite.psvfx.com>
2023-05-15 19:11:51 +0200

    Initial envs proposal vs std & vs cmake like

---
+++ env/vs/yaede-base-cli-cmk/yaede-base-cli-cmk/yaede-base-cli-cmk.cpp
@@ -0,0 +1,12 @@
+﻿// yaede-base-cli-cmk.cpp: definiuje punkt wejścia dla aplikacji.
+//
+
+#include "yaede-base-cli-cmk.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ env/vs/yaede-base-cli-cmk/yaede-base-cli-cmk/yaede-base-cli-cmk.h
@@ -0,0 +1,8 @@
+﻿// yaede-base-cli-cmk.h: plik dołączany dla standardowych systemowych plików dołączanych,
+// lub pliki dołączane specyficzne dla projektu.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: W tym miejscu przywołaj dodatkowe nagłówki wymagane przez program.
